c452b9eefb28f4e4f6810bb54f7b136fcbb04d4f
benj <benjaminacorda@gmail.com>
2025-08-16 22:40:47 -0400

    setup_script

--- setup_env.sh
+++ setup_env.sh
@@ -4,9 +4,17 @@ set -e
 echo "Updating system"
 sudo apt-get update -y
 
+echo "Installing uv"
 pip install uv
 
+echo "Creating virual environment"
 uv venv vov --python 3.12.7
-# source vov/bin/activate
+
+echo "Installing pip"
+./vov/bin/python -m ensurepip --upgrade
+
+echo "Install uv pip"
 ./vov/bin/python -m pip install uv
+
+echo "Installing packages"
 ./vov/bin/python -m uv pip install torch torchvision matplotlib noise tqdm pygame pygame_screen_recorder json torch_geometric
